8bdcc9e67e209bf04ac35f1cabe1e4682870f2d5
Thyago <chatack.tc@gmail.com>
2023-10-21 22:15:42 -0300

    [feat]: adiciona test mock que verifica função fetch

---
+++ src/mocks/city.ts
@@ -0,0 +1,17 @@
+const city = {
+  cod: 200,
+  name: 'Rio de Janeiro',
+  main: {
+    humidity: 80,
+    temp: 21,
+  },
+  weather: [{
+    description: 'overcast clouds',
+    main: 'Clear' || 'Clouds' || 'Mist' || 'Rain' || 'Thunderstorm',
+  }],
+  wind: {
+    speed: 4.12,
+  },
+};
+
+export default city;
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { vi } from 'vitest';
+import * as getWeather from '../services/getWeatherApi';
 
 import App from '../App';
+import city from '../mocks/city';
+
+afterEach(() => vi.clearAllMocks());
 
 describe('Verifica componente <App />', () => {
   it('Verifica se o componente renderiza um input', () => {
@@ -11,14 +15,6 @@ describe('Verifica componente <App />', () => {
     const input = screen.getByTestId(/input-search/i);
 
     expect(input).toBeInTheDocument();
-  });
-
-  it('Verifica se o componente renderiza um botão', () => {
-    render(<App />);
-
-    const button = screen.getByTestId(/button-search/i);
-
-    expect(button).toBeInTheDocument();
   });
 
   it('Verifica se o input recebe um texto com nome de uma cidade/país', async () => {
@@ -32,5 +28,35 @@ describe('Verifica componente <App />', () => {
     expect(input).toHaveValue('Rio de Janeiro');
   });
 
-  it('');
+  it('Verifica se o componente renderiza um botão', () => {
+    render(<App />);
+
+    const button = screen.getByTestId(/button-search/i);
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('Verifica se o botão no componente app é clicável', async () => {
+    render(<App />);
+
+    const user = userEvent.setup();
+    const button = screen.getByTestId(/button-search/i);
+
+    await user.click(button);
+  });
+
+  it('Verifica se ao procurar uma cidade a API retorna os dados corretos', async () => {
+    render(<App />);
+
+    const mockFetch = vi.spyOn(getWeather, 'default').mockResolvedValue(city);
+
+    const user = userEvent.setup();
+    const input = screen.getByTestId(/input-search/i);
+    const button = screen.getByTestId(/button-search/i);
+
+    await user.type(input, 'Rio de Janeiro');
+    await user.click(button);
+
+    expect(mockFetch).toBeCalled();
+  });
 });
